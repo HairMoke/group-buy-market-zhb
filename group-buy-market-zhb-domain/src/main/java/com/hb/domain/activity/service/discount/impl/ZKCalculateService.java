1845b6cdb78d19d882f56a68c2941265fa5a43c8
shall_zhao <shall_zhao@noreply.gitcode.com>
2025-04-08 11:14:05 +0800

    feat: 引入RabbitMQ分布式多端消费

--- group-buy-market-zhb-domain/src/main/java/com/hb/domain/activity/service/discount/impl/ZKCalculateService.java
+++ group-buy-market-zhb-domain/src/main/java/com/hb/domain/activity/service/discount/impl/ZKCalculateService.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 /**
  * 折扣
@@ -20,8 +21,8 @@ public class ZKCalculateService extends AbstractDiscountCalculateService {
         // 折扣表达式 —— 折扣的百分比
         String marketExpr = groupBuyDiscount.getMarketExpr();
 
-        // 折扣价格
-        BigDecimal deductionPrice = originalPrice.multiply(new BigDecimal(marketExpr));
+        // 折扣价格 + 四舍五入
+        BigDecimal deductionPrice = originalPrice.multiply(new BigDecimal(marketExpr)).setScale(0, RoundingMode.DOWN);
 
         // 判断折扣后金额， 最低支付一分钱
         if(deductionPrice.compareTo(BigDecimal.ZERO) <= 0){
